41ec22007f15e960c0efa6f5a877eb75069362a1
Albertinou <albertuciukas@gmail.com>
2023-03-27 13:16:14 +0300

    success page translated

--- pages/success.js
+++ pages/success.js
@@ -2,46 +2,42 @@ import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { BsBagCheckFill } from "react-icons/bs";
 
-
 import { useStateContext } from "@/context/StateContext";
 import { runFireworks } from "@/lib/utils";
 
 const Success = () => {
   const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
-  
+
   useEffect(() => {
-        localStorage.clear();
-        setCartItems([]);
-        setTotalPrice(0);
-        setTotalQuantities(0);
-        runFireworks();
+    localStorage.clear();
+    setCartItems([]);
+    setTotalPrice(0);
+    setTotalQuantities(0);
+    runFireworks();
   }, []);
-  
 
   return (
     <div className="success-wrapper">
-        <div className="success">
-            <p className="icon">
-                <BsBagCheckFill />
-            </p>
-            <h2>Ačiū, kad perkate!</h2>
-            <p className="email-msg">Check your email inbox for the receipt.</p>
-            <p className="description">
-                Jei kils klausimų, prašome kreiptis
-                <a className="email" href="mailto:order@order.lt">
-                    aidasgalkus@gmail.com
-                </a>
-            </p>
-            <Link href="/">
-                <button type="button" width="300px" className="btn">
-                    Continue Shopping
-                </button>
-            </Link>
-        </div>
+      <div className="success">
+        <p className="icon">
+          <BsBagCheckFill />
+        </p>
+        <h2>Ačiū, kad perkate!</h2>
+        <p className="email-msg">Check your email inbox for the receipt.</p>
+        <p className="description">
+          Jei kils klausimų, prašome kreiptis
+          <a className="email" href="mailto:aidasgalkus@gmail.com">
+            aidasgalkus@gmail.com
+          </a>
+        </p>
+        <Link href="/">
+          <button type="button" width="300px" className="btn">
+            Continue Shopping
+          </button>
+        </Link>
+      </div>
     </div>
-  )
-
-}
-
+  );
+};
 
 export default Success;
