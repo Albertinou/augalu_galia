9f0ef9983856475664c19e533a3dfdf0b5ed9522
Albertinou <albertuciukas@gmail.com>
2023-04-28 09:38:04 +0300

    added meta data to document JS Head

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <meta name="description" content="Augalų galia - supermaistas kepenims, imunitetui, širdžiai." />
+      <meta name="keywords" content="augalugalia, augalu galia, augalų galia, gydomasis grybas, gydomieji grybai, kova su vėžiu, imuninė sistema, supermaistas, adaptogenas,
+       antioksidantas, chaga, reishi, naudingi grybai, ilgaamžiškumas, tonikas, grybas, kovoti su vėžiu, imuninė sistema, supermaistas,
+       adaptogenas, antioksidantas, anti-uždegiminis, antivirusinis, detoksikacija, antibakterinis, antifungalinis, gijimas,
+       imuniteto stiprinimas, atjauninimas, revitalizavimas, maitinimas, balansavimas, nuotaikos stabilizavimas, raminantis,
+       širdies sveikata, stiprinimas,aukštas kraujo spaudimas, aukštas cholesterolio kiekis, kepenų ir inkstų ligos, kvėpavimo takų ligos,
+       virusinės infekcijos, ŽIV/AIDS, vėžys, parama chemoterapijos metu, nuovargis" />
+      </Head>
       <body>
         <Main />
         <NextScript />
