5062453b1abf91a70393d326a3935c31530e577a
Albertinou Albertinou <albertuciukas@gmail.com>
2024-01-21 15:40:24 +0200

    fixed navbar width

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,22 +6,20 @@ import { Cart } from './'
 import { useStateContext } from '@/context/StateContext';
 import Image from 'next/image';
 const Navbar = () => {
-  const { showCart, setShowCart, totalQuantities} = useStateContext();
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
   return (
     <div className='navbar-container'>
       <Link href="/">
-        <Image  alt= 'logo' src={'/images/augalu_galia_logo.png'} width={150} height={86} />
+        <Image alt='logo' src={'/images/augalu_galia_logo.png'} width={150} height={86} />
       </Link>
-      
-
       <button type='button'
-      className='cart-icon'
-      onClick={() => setShowCart(true)}>
+        className='cart-icon'
+        onClick={() => setShowCart(true)}>
         <AiOutlineShopping />
         <span className='cart-item-qty'>{totalQuantities}</span>
       </button>
 
-     {showCart && <Cart />}
+      {showCart && <Cart />}
 
     </div>
   )
